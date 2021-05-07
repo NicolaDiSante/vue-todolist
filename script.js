66a57b1b2a66c74bfa66cddb9e2a9287dcfeee70
NicolaDiSante <nickymacha90@gmail.com>
2021-05-07 09:13:20 +0200

    Completato con l'aiuto della registrazione e commentato

--- script.js
+++ script.js
@@ -28,10 +28,14 @@ const app = new Vue({
       alertActive: false
     
   },
+
+  // Nella funzione removeToDo devo inserire l'index perchè ho bisogno di andare ad eliminare con lo splice l'indice
   methods:{
     removeToDo(index){
       this.toDoList.splice(index,1);
     },
+
+  // Nella funzione addToDo inserisco le condizioni per le quali se la mia stringa è maggiore di 2 allora vado a pushare il valore dentro al mio array di oggetti, altrimenti faccio comprarire il mio h4
 
     addToDo(){
      if(this.strToDo.length > 2){
